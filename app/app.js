20ceaa6e19369db3d610593f9474c1a04a00dc93
elissa-murphy <elrmurph@iu.edu>
2022-09-26 10:04:52 -0400

    test

--- app/app.js
+++ app/app.js
@@ -4,19 +4,19 @@ $(document).ready(function () {
 
 $(window).on("load", function () {
   console.log("Loaded");
-  Swal.fire({
-    title: "<strong>HTML <u>example</u></strong>",
-    icon: "info",
-    html:
-      "You can use <b>bold text</b>, " +
-      '<a href="//sweetalert2.github.io">links</a> ' +
-      "and other HTML tags",
-    showCloseButton: true,
-    showCancelButton: true,
-    focusConfirm: false,
-    confirmButtonText: '<i class="fa fa-thumbs-up"></i> Great!',
-    confirmButtonAriaLabel: "Thumbs up, great!",
-    cancelButtonText: '<i class="fa fa-thumbs-down"></i>',
-    cancelButtonAriaLabel: "Thumbs down",
-  });
+  //   Swal.fire({
+  //     title: "<strong>HTML <u>example</u></strong>",
+  //     icon: "info",
+  //     html:
+  //       "You can use <b>bold text</b>, " +
+  //       '<a href="//sweetalert2.github.io">links</a> ' +
+  //       "and other HTML tags",
+  //     showCloseButton: true,
+  //     showCancelButton: true,
+  //     focusConfirm: false,
+  //     confirmButtonText: '<i class="fa fa-thumbs-up"></i> Great!',
+  //     confirmButtonAriaLabel: "Thumbs up, great!",
+  //     cancelButtonText: '<i class="fa fa-thumbs-down"></i>',
+  //     cancelButtonAriaLabel: "Thumbs down",
+  //   });
 });
